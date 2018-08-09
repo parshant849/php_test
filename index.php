5f6aa21dc8d38a5cf62ecffcf3a455e568901070
Parshant <parshant@ucreate.co.in>
2018-08-09 10:58:36 +0530

    Change in index.php

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php 
-echo 'sdfsdf';
-phpinfo();
+$url = parse_url(getenv("DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$config = array(
+    'host' => $server ,
+    'user' => $username ,
+    'pw' => $password,
+    'db' => $db 
+);
+
+//echo 'sdfsdf';
+//phpinfo();
 ?>
